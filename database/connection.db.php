f433a2b125c528b853e56a75ffab5f2fe279e7ed
Matilde <up202108853@g.uporto.pt>
2023-05-21 22:52:46 +0100

    database connection fixed

--- database/connection.db.php
+++ database/connection.db.php
@@ -2,7 +2,7 @@
     declare(strict_types = 1);
 
     function getDatabaseConnection(): PDO {
-        $db = new PDO('sqlite:'. __DIR__ . '/../database/ltw_database.sql');
+        $db = new PDO('sqlite:'. __DIR__ . '/../database/basedadps.db');
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
         $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
